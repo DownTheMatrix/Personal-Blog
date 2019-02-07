dc02e6740a15ec8a873abc9c581818013b98d1d3
DownTheMatrix <suija87@gmail.com>
2019-02-07 18:44:13 +0100

    Add input field

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,47 @@
 import React from "react";
+import portrait from "../img/portrait.svg";
 
 const Contact = (props) => {
     return (
-        <div>
-            <h2>GOT QUESTIONS?</h2>
-            <p>The easiest thing to do is post on
-            our <a href="http://forum.kirupa.com">forums</a>.
-            </p>
-      </div>
+        <section id="contact">
+
+            <div className="contact-portrait">
+                <img src={portrait} alt=""/>
+            </div>
+        
+            <div className="form-container container">
+
+                <form action="#" className="contact-form">
+                    <div className="field">
+                        <label htmlFor="name">First Name</label>
+                        <input type="text" id="name" required />
+                    </div>
+
+                    <div className="field">
+                        <label htmlFor="name">Last Name</label>
+                        <input type="text" id="name" required />
+                    </div>
+
+                    <div className="field">
+                        <label htmlFor="email">Company</label>
+                        <input type="text" id="company" required />
+                    </div>
+
+                    <div className="field">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" required />
+                    </div>
+
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="5"></textarea>
+                    </div>
+
+                    <button className="contact-btn">Submit &rarr;</button>
+                </form>
+
+            </div>
+        </section>
     );
 }
 
