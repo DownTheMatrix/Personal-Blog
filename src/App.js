ee80e31902ae170445a7e6950a6ad40eacdbcd7b
DownTheMatrix <suija87@gmail.com>
2019-02-07 13:03:00 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import "./css/style.css";
+import Home from "./Home";
+import Articles from "./Articles";
+import Contact from "./Contact";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <h1>Simple SPA</h1>
+            <ul className="header">
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to="/articles">Articles</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+            <div className="content">
+              <Route exact path="/" component={Home} />
+              <Route path="/articles" component={Articles} />
+              <Route path="/contact" component={Contact} />
+            </div>
+        </div>
+      </HashRouter>
     );
   }
 }
