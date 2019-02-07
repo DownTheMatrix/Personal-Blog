b3d7f850b207e362db7ac76d99c5eb717aeaaf60
DownTheMatrix <suija87@gmail.com>
2019-02-07 15:56:48 +0100

    Add text content

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const Home = (props) => {
     return (
-        <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-      </div>
+        <div className="home-container container">
+            <div className="home-text">
+                <div className="inner-grid">
+                    <h2>Welcome to my blog site!</h2>
+                    <p>After procrastrinating for (too) long, I finally decided to start my own blog. The enthusiasm in this case is doubled as I not only will provide the content for the blog, but I'm also responsible for the creation of this small website! If you want to know, I made it by using CSS Grid (which I wish I'd used it more in my past projects) and some React Router magic.</p>
+                    <p>My name is Bruno, I was born in Rome, Italy, and for most of my life my interests and passions mainly revolved around cinema, literature and languages. In 2011 I got my degree in Asian Languages (Japanese and Mandarin Chinese), and right after I travelled to China to further develop my skills and to test myself in such a huge and unknown environment.</p>
+                    <p>The experiment went well, and there I studied at different universities (Bejing and Kunming), explored the country, enjoyed breath-taking landscapes, tasted great food and met with some of the finest people in the world. I also worked as a teacher (teaching Italian and Latin to local students... now, that was tough!), as a translator and interpreter, and finally as a sales supervisor at a major import company.</p>
+                    <p>I started my journey as a Web Developer in late 2017 by taking advantage of the incredible opportunity that was given to me by the Google/Udacity Front-End Developement Nanodegree Scholarship, and I never stopped learning new and amazing things since then. Despite a (not too)few moments of frustration, I can definitely regard this crazy choice as something that changed my life for the better.</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
