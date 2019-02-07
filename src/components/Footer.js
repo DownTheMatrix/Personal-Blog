36f2ebd9bcb0b16fe9bec05b3b36e358b8d776ee
DownTheMatrix <suija87@gmail.com>
2019-02-07 15:58:45 +0100

    Create components folder

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = (props) => {
+    return (
+        <footer id="footer">
+            <p>Bruno Mazza &copy; 2019, All Rights Reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
